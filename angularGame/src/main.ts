87fb4989e2bad5f3c8ad1cbe6cb168b1cbd0c837
mbm60-max <mbm60@bath.ac.uk>
2024-04-08 16:47:00 +0100

    config

--- angularGame/src/main.ts
+++ angularGame/src/main.ts
@@ -2,9 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
-if (!environment.production) {
-  // Load environment variables from .env file in non-production environments
-  require('dotenv').config();
-}
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
